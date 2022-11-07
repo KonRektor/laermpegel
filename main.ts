569d5f8882376b5d33744adec90b98b0cba234e7
KonRektor <105855709+KonRektor@users.noreply.github.com>
2022-11-07 19:39:50 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    if (input.soundLevel() < 50) {
+        basic.setLedColor(0xff0000)
+        basic.showIcon(IconNames.Sad)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    } else {
+        basic.setLedColor(0x00ff00)
+        basic.showIcon(IconNames.Happy)
+    }
+})
